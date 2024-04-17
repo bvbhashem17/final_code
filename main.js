091f9345c38977a8abd040d1e43bf16903c8b1de
MENNa2027 <153994088+MENNa2027@users.noreply.github.com>
2024-04-17 22:58:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,73 +1,59 @@
-import * as THREE from "three";
-import { World } from "./world";
-import { Box } from "./Box";
-import { ArcballControls } from "three/addons/controls/ArcballControls.js";
-import { Loaders } from "./loaders";
 
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 5;
+import * as THREE from 'three'
+import {LightManager} from './try.js';
+const canvas =document.querySelector('canvas.webgl');
 
-// ========================================
-let box = new Box({
-  geo: {
-    width: 1,
-    height: 1,
-    depth: 1,
-  },
-  material: new THREE.MeshBasicMaterial({ color: "red" }),
-  position: [0, 0, 0],
-  castShadow: true,
-}).getMesh();
+const scene = new THREE.Scene();
+// cube 
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material=new THREE.MeshStandardMaterial({color:'red'});
+const mesh = new THREE.Mesh(geometry,material)
+scene.add(mesh)
 
-// ==========================================
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+// camera 
+const sizes={
+  width:window.innerWidth,
+  height:window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
+camera.position.z=3
+camera.lookAt(mesh.position)
+scene.add(camera)
 
-// ==============
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(-5, 5, 0);
-// ==========================================
-let loaders = new Loaders();
-let fox = await loaders.asyncLoadGLTFModel({
-  modelPath: "./models/Fox/glTF/Fox.gltf",
-  isCompressed: true,
-});
-fox.scene.position.y = 0.5;
-fox.scene.scale.set(0.015, 0.015, 0.015);
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas
+})
+renderer.setSize(sizes.width,sizes.height)
 
-World({
-  canvasDOMElement: document.querySelector("#app"),
-  style: {
-    bgColor: "black",
-    width: window.innerWidth,
-    height: window.innerHeight,
-    opacity: 0.8,
-  },
-  camera: camera,
-  childrens: [box, fox.scene],
-  lights: [ambientLight, directionalLight],
-  haveOrbitControls: false,
-  isResponsive: true,
-  loop: (c) => {
-    // console.log(c);
-  },
-  addControls: (r, c, s) => {
-    const controls = new ArcballControls(c, r.domElement, s);
+// light 
+const lig = new LightManager(scene);
+const pointLightParams ={
+  type:'point',
+  params:{
+    color:0x00fff0,
+    intensity:6,
+    position:{x:0,y:0,z:3},
+    distance:10
+  }
+}
 
-    controls.addEventListener("change", function () {
-      r.render(s, c);
-    });
-  },
-});
-World();
+////
+const rectAreaLightParams={
+  type:'rect',
+  params:{
+    color:0xffffff,
+    intensity:1,
+    width:2,
+    height:2,
+    position:{x:0,y:0,z:0},
+    normal:{x:3,y:1,z:3},// facing up is default
+    lookAt:{x:0,y:0,z:0}
+    
+  }
+};
+// add lights to the scene 
+lig.addLights(pointLightParams);
+lig.addLights(rectAreaLightParams);
+renderer.render(scene,camera)
+
